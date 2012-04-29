3276d3fa79683770f7722e3aa72c3046324a3bec
Bruno Martins <brunoseivam@gmail.com>
2012-04-29 15:18:37 -0300

    build_packet

--- src/main.c
+++ src/main.c
@@ -33,103 +33,87 @@ enum command
 
 
 unsigned char state, command;
-int timeout;
+char			  own_ip[4];
 
-void alarm_handler(int signumber)
+const struct timeval STARTUP_TIMEOUT =
 {
-   timeout = 1;
-   printf("ALARM!\n");
+	.tv_sec = 5,
+	.tv_usec = 0
+};
+
+const struct timeval SLAVE_TIMEOUT =
+{
+	.tv_sec = 5,
+	.tv_usec = 0
+};
+
+const struct timeval CANDIDATE_TIMEOUT =
+{
+	.tv_sec = 5,
+	.tv_usec = 0
+};
+
+const struct timeval MASTER_TIMEOUT =
+{
+	.tv_sec = 5,
+	.tv_usec = 0
+};
+
+const struct timeval ACCEPT_TIMEOUT =
+{
+	.tv_sec = 5,
+	.tv_usec = 0
+};
+
+void build_packet(struct packet *pkt, unsigned char cmd, int put_time)
+{
+	pkt->type = cmd;
+	pkt->version = 1;
+	pkt->seqnum = 0;
+
+	if(put_time)
+		gettimeofday(&pkt->time, NULL);
+	else
+	{
+		pkt->time.tv_sec = 0;
+		pkt->time.tv_usec = 0;
+	}
+
 }
+
+void set_timer(struct timeval* tval, struct timeval timeout)
+{
+	tval->tv_sec = timeout.tv_sec;
+	tval->tv_usec = timeout.tv_usec;
+}
+
 
 int main(int argc, char **argv)
 {
-   /*struct sigaction signal_action;*/
-   struct timeval tval;
-	struct packet pkt;
+   struct timeval	tval;
+	int				timeout = 0;
+	struct packet	pkt;
 
 	/* Open a socket and start listening to a scpefied port */
 	open_udp_socket();
 
-	if(argc < 3){
-		fprintf(stderr, "Error: usage udp_test [client|server] <dest_ip>\n");
-		return 0;
-	}
-
-/*	printf("unsigned char size = %d\n", sizeof(unsigned char));
-	printf("unsigned short size = %d\n", sizeof(unsigned short));
-	printf("long int size = %d\n", sizeof(long int));
-	printf("long size = %d\n", sizeof(long));
-	printf("tval.tv_sec = %d\n", sizeof(tval.tv_sec));
-	printf("tval.tv_usec = %d\n", sizeof(tval.tv_usec));
-	printf("struct timeval size = %d\n", sizeof(struct timeval));
-	printf("packet size = %d\n", sizeof(struct packet));
-	printf("unsigned long= %d\n", sizeof(unsigned long));*/
-
-	/* Client */
-	if(!strcmp(argv[1], "client")){
-		short count = 0;
-		while(1){
-			pkt.type = 0xAA;
-			pkt.version = 0xBB;
-			pkt.seqnum = count;
-			gettimeofday(&pkt.time, NULL);
-			strncpy((char*) pkt.ip, "ipip", 4);
-			printf("-------------------------\n");
-			print_packet(&pkt);
-			printf("-------------------------\n");
-			send_msg(argv[2], &pkt);
-			count++;
-			sleep(1);
-		}
-	}
-	/* Server */
-	else{
-		char src_addr[4];
-		while(1){
-			tval.tv_usec = 0;
-			tval.tv_sec = 5;
-			recv_msg(src_addr, &pkt, &tval);
-			printf("-------------------------\n");
-			print_msg(src_addr, &pkt, &tval);
-			printf("-------------------------\n");
-		}
-	}
-
-   /*tval.it_interval.tv_sec = 5;
-   tval.it_interval.tv_usec = 0;
-
-   tval.it_value.tv_sec = 5;
-   tval.it_value.tv_usec = 0;
-
-   signal_action.sa_handler = alarm_handler;
-   sigemptyset(&signal_action.sa_mask);
-   signal_action.sa_flags = 0;
-
-   sigaction(SIGALRM, &signal_action, NULL);
-
-   setitimer(ITIMER_REAL, &tval, NULL);
-
-   // setup code:
-
-   state = STATE_STARTUP;
-
-   send(CMD_MASTERREQ);
-   //set nomaster timer
-
+	/* Startup: */
+   state = STATE_STARTUP;					/* Set STARTUP state   */
+	send(CMD_MASTERREQ);						/* Search for a master */
+	set_timer(&tval, STARTUP_TIMEOUT);	/* Set timeout         */
 
    for(;;)
    {
-      if(timeout)
-      {
-         command = CMD_TIMEOUT;
-         timeout = 0;
-      }
-      else
-         recv(command);
+      command = recv(command);
 
       switch(state | command)
       {
-         case(STATE_SLAVE | CMD_TIMEOUT):
+         case(STATE_STARTUP | CMD_TIMEOUT):
+            send(CMD_MASTERUP);
+            state = STATE_MASTER;
+            break;
+
+         /*case(STATE_SLAVE | CMD_TIMEOUT):
             send(CMD_ELECTION);
             // set candidate timer
             state = STATE_CANDIDATE;
@@ -137,11 +121,6 @@ int main(int argc, char **argv)
 
          case(STATE_SLAVE | CMD_ADJTIME):
             adjust_time();
-            break;
-
-         case(STATE_STARTUP | CMD_TIMEOUT):
-            send(CMD_MASTERUP);
-            state = STATE_MASTER;
             break;
 
          case (STATE_STARTUP | CMD_MASTERUP):
@@ -178,7 +157,7 @@ int main(int argc, char **argv)
          case (STATE_MASTER | CMD_TIMEOUT):
             collect_clocks();
             send_adjtimes();
-            break;
+            break;*/
       }
    }
 
@@ -187,8 +166,61 @@ int main(int argc, char **argv)
      printf(".");
      fflush(stdout);
      sleep(1);
-   }*/
+   }
 
 
    return 0;
 }
+/*	printf("unsigned char size = %d\n", sizeof(unsigned char));
+	printf("unsigned short size = %d\n", sizeof(unsigned short));
+	printf("long int size = %d\n", sizeof(long int));
+	printf("long size = %d\n", sizeof(long));
+	printf("tval.tv_sec = %d\n", sizeof(tval.tv_sec));
+	printf("tval.tv_usec = %d\n", sizeof(tval.tv_usec));
+	printf("struct timeval size = %d\n", sizeof(struct timeval));
+	printf("packet size = %d\n", sizeof(struct packet));
+	printf("unsigned long= %d\n", sizeof(unsigned long));*/
+
+	/* Client *
+	if(!strcmp(argv[1], "client")){
+		short count = 0;
+		while(1){
+			pkt.type = 0xAA;
+			pkt.version = 0xBB;
+			pkt.seqnum = count;
+			gettimeofday(&pkt.time, NULL);
+			strncpy((char*) pkt.ip, "ipip", 4);
+			printf("-------------------------\n");
+			print_packet(&pkt);
+			printf("-------------------------\n");
+			send_msg(argv[2], &pkt);
+			count++;
+			sleep(1);
+		}
+	}
+	/* Server *
+	else{
+		char src_addr[4];
+		while(1){
+			tval.tv_usec = 0;
+			tval.tv_sec = 5;
+			recv_msg(src_addr, &pkt, &tval);
+			printf("-------------------------\n");
+			print_msg(src_addr, &pkt, &tval);
+			printf("-------------------------\n");
+		}
+	}
+
+   /*tval.it_interval.tv_sec = 5;
+   tval.it_interval.tv_usec = 0;
+
+   tval.it_value.tv_sec = 5;
+   tval.it_value.tv_usec = 0;
+
+   signal_action.sa_handler = alarm_handler;
+   sigemptyset(&signal_action.sa_mask);
+   signal_action.sa_flags = 0;
+
+   sigaction(SIGALRM, &signal_action, NULL);
+
+   setitimer(ITIMER_REAL, &tval, NULL);*/
